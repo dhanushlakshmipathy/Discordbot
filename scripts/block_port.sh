0283096c3352a6f99a386fa16a3f2c209056ccae
dhanushlakshmipathy <lakshmipathydhanush@gmail.com>
2025-11-09 12:01:10 +0530

    Fix iptables command to block TCP port

--- scripts/block_port.sh
+++ scripts/block_port.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 PORT=$1
-sudo iptables -I ufw-before-input -p tcp --dport "$PORT" -j DROP;
-sudo netfilter-persistent save
+sudo iptables -I INPUT -p tcp --dport ${PORT} -j DROP
 echo "🧱 Blocked TCP port $PORT."
